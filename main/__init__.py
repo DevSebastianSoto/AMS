76962b3f1c5625486e5865e66b59c53e3fd2cb65
Sebastian Soto Madrigal <dev.snsm@gmail.com>
2023-05-21 19:26:54 -0600

    app.after_request solution

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,17 @@
 import os
+
 import jinja2
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, flash, redirect, render_template, url_for
 
 app = Flask(__name__)
+
+
+@app.after_request
+def apply_caching(response):
+    response.headers["X-Frame-Options"] = "SAMEORIGIN,www.facebook.com"
+    return response
+
+
 app.config['SECRET_KEY'] = "bda15c5adda26a64d0015dec0788894e"
 
 template_dir = os.path.join('.', 'templates')
@@ -10,3 +19,4 @@ loader = jinja2.FileSystemLoader(template_dir)
 environment = jinja2.Environment(loader=loader)
 
 from main import routes
+
